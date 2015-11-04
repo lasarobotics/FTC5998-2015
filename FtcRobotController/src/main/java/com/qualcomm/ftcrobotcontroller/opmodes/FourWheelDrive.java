7c0111a2359f7f81d7b5997c3a4dfc53a2c1af34
Ojas Ahuja <OjasA@users.noreply.github.com>
2015-11-04 16:39:58 -0600

    Add Lift, Intake
    Need to lint this

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FourWheelDrive.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FourWheelDrive.java
@@ -9,23 +9,43 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class FourWheelDrive extends OpMode {
 
     DcMotor frontLeft, frontRight, backLeft, backRight;
+    DcMotor lift, intake;
     Controller firstController;
 
     public void init() {
+        gamepad1.setJoystickDeadzone(.1F);
+        gamepad2.setJoystickDeadzone(.1F);
         frontLeft = hardwareMap.dcMotor.get("frontLeft");
         frontRight = hardwareMap.dcMotor.get("frontRight");
         backLeft = hardwareMap.dcMotor.get("backLeft");
         backRight = hardwareMap.dcMotor.get("backRight");
+        lift = hardwareMap.dcMotor.get("lift");
+        intake = hardwareMap.dcMotor.get("intake");
 
         firstController = new Controller(gamepad1);
+        secondController = new Controller(gamepad2);
     }
 
     public void loop() {
         firstController.update(gamepad1);
+        secondController.update(gamepad2);
         Tank.motor4(frontLeft, frontRight, backLeft, backRight, firstController.left_stick_y, firstController.right_stick_y);
+        if (secondController.a == 1) {
+            intake.setPower(1);
+        }
+        else if (secondController.x == 1) {
+            intake.setPower(-1);
+        }
+        if (secondController.b == 1) {
+            lift.setPower(1);
+        }
+        else if (secondController.y == 1) {
+            lift.setPower(-1);
+        }
+        
     }
 
     public void stop() {
-        System.out.println("");
+        
     }
 }
