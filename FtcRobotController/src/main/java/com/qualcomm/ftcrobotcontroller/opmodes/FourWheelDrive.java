c8914592aba184571339c7c4b0324372eda1c13f
Ojas Ahuja <kingbobthefifth@gmail.com>
2015-11-30 22:00:32 -0600

    Add Goal Servo

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FourWheelDrive.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FourWheelDrive.java
@@ -15,7 +15,7 @@ public class FourWheelDrive extends OpMode {
     Servo liftServo;
     Controller firstController;
     Controller secondController;
-    Servo hanger;
+    Servo hanger, goal;
 
     public void init() {
         gamepad1.setJoystickDeadzone(.1F); // make sure we don't get fake values
@@ -28,10 +28,12 @@ public class FourWheelDrive extends OpMode {
         lift = hardwareMap.dcMotor.get("lift");
         liftServo = hardwareMap.servo.get("liftServo");
         hanger = hardwareMap.servo.get("hanger");
+        goal = hardwareMap.servo.get("goal");
         firstController = new Controller(gamepad1);
         secondController = new Controller(gamepad2);
         liftServo.setPosition(0.5); // set the servo halfway in between 0 and 1, so there can be
         // 10 increments on either side
+        goal.setPosition(0.5);
     }
 
     public void loop() {
@@ -39,6 +41,12 @@ public class FourWheelDrive extends OpMode {
         secondController.update(gamepad2);
         Tank.motor4(frontLeft, frontRight, backLeft, backRight, -firstController.left_stick_y,
                 firstController.right_stick_y);
+
+        if (firstController.x == ButtonState.PRESSED) {
+            goal.setPosition(MathUtil.coerce(0.0, 1.0, goal.getPosition() + 0.20));
+        } else if (firstController.b == ButtonState.PRESSED) {
+            goal.setPosition(MathUtil.coerce(0.0, 1.0, goal.getPosition() - 0.20));
+        }
 
         if (secondController.dpad_up == ButtonState.PRESSED) {
             intake.setPower(1);
